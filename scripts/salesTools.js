4df08fdd3006e95e8007668d175c6e7b83fcd550
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-09-12 13:11:25 -0400

    Add Youtube embed feature to videos for cleanre look

--- scripts/salesTools.js
+++ scripts/salesTools.js
@@ -8,3 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
       salesToolsPlaceholder.innerHTML = data;
     });
 });
+
+var players = [];
+
+function onYouTubeIframeAPIReady() {
+  var allVideos = document.querySelectorAll(".video-link iframe");
+
+  allVideos.forEach(function (vid, index) {
+    var player = new YT.Player(vid, {
+      events: {
+        onStateChange: onPlayerStateChange,
+      },
+    });
+    players.push(player);
+  });
+}
+
+function onPlayerStateChange(event) {
+  var videoContainer = event.target.getIframe().closest(".video-link");
+  var description = videoContainer.querySelector(".video-description");
+
+  // YT.PlayerState.PLAYING == 1
+  if (event.data == YT.PlayerState.PLAYING) {
+    description.style.opacity = "0";
+  } else {
+    description.style.opacity = "1";
+  }
+}
