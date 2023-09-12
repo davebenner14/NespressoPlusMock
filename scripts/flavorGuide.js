0c2947b9a03e6db1dfa4d59beac2e9d3ba987969
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-09-12 15:07:18 -0400

    Update flavor guide section

--- scripts/flavorGuide.js
+++ scripts/flavorGuide.js
@@ -7,6 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((response) => response.text())
     .then((data) => {
       flavorGuidePlaceholder.innerHTML = data;
+
+      const container = document.querySelector(".flavor-container");
+
+      document
+        .getElementById("left-arrow")
+        .addEventListener("click", function () {
+          container.scrollLeft -= 200;
+        });
+
+      document
+        .getElementById("right-arrow")
+        .addEventListener("click", function () {
+          container.scrollLeft += 200;
+        });
     })
     .catch((error) => {
       console.error("Error fetching the flavor guide:", error);
